1e221c28ebdd72b9734af89d2820e2c707a0f675
Arnav Sinha <arnav24keshav@gmail.com>
2025-06-02 00:12:19 +0530

    Update final_nlp_contract_analyzer_model.py

--- final_nlp_contract_analyzer_model.py
+++ final_nlp_contract_analyzer_model.py
@@ -6,16 +6,20 @@ import datetime
 from datetime import date, time, datetime, timedelta
 import os
 from dateutil import parser
+
+days_before_expire=3 #specify the number of days to consider as expiring soon
+
 ogpath="C:/Users/krish/Desktop/Document1(1).pdf" #file path 
 expiry_folder="C:/Users/krish/Desktop/expires" #epiry folder path
 nonexpiry_folder="C:/Users/krish/Desktop/not expire" #non expiry folder path
+no_exp_date="" #folder to save for no expiration date
 
 fname=os.path.basename(ogpath)
 expirypath=os.path.join(expiry_folder,fname)
 nonexpirypath=os.path.join(nonexpiry_folder,fname)
 pdf=fitz.open(ogpath)
 
-flag="not saved"
+flag="not saved" #flag for saving the file
 
 dtoday=datetime.now()
 
@@ -24,42 +28,63 @@ dtoday=datetime.now()
 # print(dtoday.month)
 
 nlp = spacy.load("en_core_web_md")
+exp_sim_list = ["expire", "end", "valid", "terminate"] #list containing words similar to expire 
 
 for page in pdf:
     text=page.get_text()
     #preprocessing text to remove ordinals
-    norm_text=re.sub(r'(\d+)(st|nd|rd|th)', r'\1', text)
-    exp_sim_list = ["expire", "end", "valid", "terminate"]
-    final_simlist=[]
+    norm_text=re.sub(r'(\d+)(st|nd|rd|th)', r'\1', text) 
+
+    final_simlist=[] #list containing words similar to all words given inside above list to check for in document
+
     exp_date=""
-    for your_word in exp_sim_list:
+    #adding similar words to final list
+    for your_word in exp_sim_list: 
         ms = nlp.vocab.vectors.most_similar(np.asarray([nlp.vocab.vectors[nlp.vocab.strings[your_word]]]), n=20)
         words_list = [nlp.vocab.strings[w] for w in ms[0][0]]
         final_simlist.extend(words_list)
+        print("searching for words: ",final_simlist)
+    #reading and extracting tokens and recognizing date entities
     doc = nlp(norm_text)
     for token in doc:
-        if token.lemma_ in final_simlist:
-            sub_sent = " ".join([t.text for t in token.subtree])
-            # print(sub_sent)
-            sub_sent_wordlist=list(token.subtree)
-            span=doc[sub_sent_wordlist[0].i:sub_sent_wordlist[-1].i+1]
-            for tok in span.ents:
-                if tok.label_=="DATE":
-                    # print(tok)
-                    exp_date=tok.text
-            # print("final found exp_date=",exp_date)
-            dt = parser.parse(exp_date)
-            # print(dt)
-            # print("\n")
-            dleft=(dt-dtoday).days
-            if dleft<100:
-                pdf.save(expirypath)
-                # print("days left:",dleft)
-                flag="saved"
-                break
-    if flag!="saved":
-        pdf.save(nonexpirypath)
+        try:
+            if token.lemma_ in final_simlist: #finding word root in documents that matches the final list of words
+                sub_sent = " ".join([t.text for t in token.subtree])
+                sub_sent_wordlist=list(token.subtree)
+                span=doc[sub_sent_wordlist[0].i:sub_sent_wordlist[-1].i+1]
+                for tok in span.ents:
+                    if tok.label_=="DATE":
+                        exp_date=tok.text
+                try:
+                    dt = parser.parse(exp_date)
+                    dleft=(dt-dtoday).days
+                    print("todays date: ",dtoday)
+                    print("expiry date: ",dt)
+                    print("days left for this contract to expire: ",dleft)
+                    #comparing dates and saving file
+                    try:
+                        if dleft<days_before_expire:
+                            pdf.save(expirypath)
+                            print("agreement about to expire soon")
+                            flag="saved"
+                            print("saved file",fname,"in expiry folder",expiry_folder)
+                            break
+                    except:
+                        print("error saving file")
+                        flag="saved"
+                except:
+                    print("error processing found date")
+                    flag="saved"#so that no furher processing is done to the pdf
+        except:
+            print("no expiration date found.")
+            pdf.save(no_exp_date)
+            print("file",fname,"saved in no expiration dates found folder",no_exp_date)
+    
+    try:            
+        if flag!="saved":
+            pdf.save(nonexpirypath)
+            print("pdf not expiring soon")
+            print("saved file",fname,"in nonexpiry_folder",nonexpiry_folder)
+    except:
+        print("error saving file")
 pdf.close()
-
-
-print(words)
